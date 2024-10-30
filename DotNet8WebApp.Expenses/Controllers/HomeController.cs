b431305ae60fcf88dda62a3ada2e66f2ad947370
Myo Thet <mr.myothet@gmail.com>
2024-10-30 20:25:08 +0700

    Create - new Expense

--- DotNet8WebApp.Expenses/Controllers/HomeController.cs
+++ DotNet8WebApp.Expenses/Controllers/HomeController.cs
@@ -12,11 +12,13 @@ namespace MyoThetDotNetCore.Expenses.Controllers
         private readonly ILogger<HomeController> _logger;
 
         private readonly IConfiguration _configuration;
+        private readonly string _dbConnectionString;
 
         public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
             _configuration = configuration;
+            _dbConnectionString = _configuration.GetConnectionString("DbConnection")!;
         }
 
         public async Task<IActionResult> IndexAsync()
@@ -51,6 +53,66 @@ namespace MyoThetDotNetCore.Expenses.Controllers
             {
                 throw new Exception(ex.Message);
             }
+        }
+
+        public IActionResult NewExpense()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SaveExpense(ExpenseModel requestModel)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(_dbConnectionString);
+                await connection.OpenAsync();
+
+                string query =
+                    @"INSERT INTO [dbo].[Tbl_Expense]
+           ([Title]
+           ,[MoneySpent]
+           ,[SpentDate]
+           ,[Category]
+           ,[IsDeleted])
+     VALUES
+           (@Title
+           ,@MoneySpent
+           ,@SpentDate
+           ,@Category
+           ,@IsDeleted)";
+
+                List<SqlParameter> parameters = new List<SqlParameter>()
+                {
+                    new SqlParameter("@Title", requestModel.Title),
+                    new SqlParameter("@MoneySpent", requestModel.MoneySpent),
+                    new SqlParameter("@SpentDate", requestModel.SpentDate),
+                    new SqlParameter("@Category", requestModel.Category),
+                    new SqlParameter("@IsDeleted", requestModel.IsDeleted)
+                };
+
+                SqlCommand command = connection.CreateCommand();
+                command.CommandText = query;
+                command.Parameters.AddRange(parameters.ToArray());
+                int result = await command.ExecuteNonQueryAsync();
+                await connection.CloseAsync();
+
+                if (result > 0)
+                {
+                    TempData["success"] = "Saving successful.";
+                }
+
+                if (result == 0)
+                {
+                    TempData["error"] = "Saving Failed.";
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return RedirectToAction("Index");
         }
 
         public IActionResult Privacy()
